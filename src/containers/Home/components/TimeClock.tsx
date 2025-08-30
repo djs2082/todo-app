512f1b2b83a8115e8d09908aa2ef2299044f0932
djs2082 <Dilip.Joshi@solarwinds.com>
2025-08-30 15:06:57 +0530

    Fixed Eslint Issue

--- src/containers/Home/components/TimeClock.tsx
+++ src/containers/Home/components/TimeClock.tsx
@@ -96,7 +96,7 @@ export default function TimeClock({ value, onChange, size = 220 }: TimeClockProp
       window.removeEventListener('mousemove', onMove);
       window.removeEventListener('mouseup', onUp);
     };
-  }, [dragging, mode]);
+  }, [dragging, mode, handlePointer]);
 
   const hour12 = ((hour % 12) + 12) % 12 || 12;
   const hourAngle = (hour12 % 12) * 30; // 0..330
